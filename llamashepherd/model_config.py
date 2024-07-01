03c37a482e5580313f82fcf9f61e59b7c73601a3
Fan Mo <fan.mo-4@student.manchester.ac.uk>
2024-07-01 13:23:53 +0000

    Improved downloading of multiple implementations and models
    1. Separate the download of implementations and models \
    2. Multiple contents can be chosen and downloaded at once\
    3. Language and model name can be specified

--- llamashepherd/model_config.py
+++
@@ -1,8 +0,0 @@
-# model_config.py
-
-urls = {
-    "stories15M": "https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin",
-    "stories42M": "https://huggingface.co/karpathy/tinyllamas/resolve/main/stories42M.bin",
-    "stories110M": "https://huggingface.co/karpathy/tinyllamas/resolve/main/stories110M.bin",
-    "tokenizer": "https://github.com/karpathy/llama2.c/raw/master/tokenizer.bin"
-}
